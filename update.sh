763c0f6448daaf30fb61c9116b68c8dc70e927a9
Espen Olsen <eo@friendup.cloud>
2018-10-26 14:36:02 +0200

    patch/better_update_script * now saves non-default fup path to 'fup.path' and reads it on next run

--- update.sh
+++ update.sh
@@ -4,36 +4,53 @@
 # Copies the modified files in the friendchat/server directory to
 # the proper location in Friend build directory structure.
 
-# Edit this line if your Friend directory is not the same as this one
-FRIEND="/home/$USER/friendup"
-
 echo "Stopping presence-server system service"
 sudo systemctl stop presence-server
 
+FRIEND=""
+
+if [ -f "fup.path" ]; then
+    FRIEND=`cat fup.path`
+    echo "found fup.path: $FRIEND"
+fi
+
+if [ -z "$FRIEND" ]; then
+    FRIEND="/home/$USER/friendup"
+fi
+
+echo "FRIEND: $FRIEND"
+FRIEND_CHECK=$FRIEND
+
 # Eventually asks for the good directory
-if [ ! -f "$FRIEND/build/cfg/cfg.ini" ]; then
+if [ ! -f "$FRIEND_CHECK/build/cfg/cfg.ini" ]; then
     while true; do
-        temp=$(dialog --backtitle "Friend Chat client update" --inputbox "\
-Please enter the path to the FriendUP directory." 11 60 "$FRIEND" --output-fd 1)
+        temp=$(dialog --backtitle "Friend Chat update" --inputbox "\
+Please enter the path to the FriendUP directory." 11 60 "$FRIEND_CHECK" --output-fd 1)
         if [ $? = "1" ]; then
-            clear
+            #clear
             echo "Update aborted."
             exit 1
         fi
         if [ $temp != "" ]; then
-            FRIEND="$temp"
+            FRIEND_CHECK="$temp"
         fi
-
+        
         # Verifies the directory
-        if [ ! -f "$FRIEND/build/cfg/cfg.ini" ]; then
+        if [ ! -f "$FRIEND_CHECK/build/cfg/cfg.ini" ]; then
             dialog --backtitle "Friend Chat client update" --msgbox "\
 Friend was not found in this directory,\n\
 or Friend was not properly installed." 10 50
         else
-            clear
+            #clear
             break;
         fi
     done
+fi
+
+if [ "$FRIEND" != "$FRIEND_CHECK" ]; then
+    echo "new path found: $FRIEND_CHECK"
+    echo "$FRIEND_CHECK" > fup.path
+    FRIEND="$FRIEND_CHECK"
 fi
 
 # Creates destination directory if it does not exist
